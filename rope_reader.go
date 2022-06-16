90abb86b49f41ddf781cf1c3e59549b2cf61c042
Rob King <rob@frigidriver.com>
2022-06-16 17:39:47 -0500

    More comments.

--- rope_reader.go
+++ rope_reader.go
@@ -4,15 +4,20 @@ import (
 	"io"
 )
 
+// A Reader provides an implementation of io.Reader for ropes.
 type Reader struct {
 	rope     *Rope
 	position int64
 }
 
+// Return a new reader attached to the given rope.
 func NewReader(rope *Rope) *Reader {
 	return rope.Reader()
 }
 
+// Read implements the standard Read interface:
+// it reads data from the rope, populating p, and returns
+// the number of bytes actually read.
 func (reader *Reader) Read(p []byte) (n int, err error) {
 	n, err = reader.rope.ReadAt(p, reader.position)
 	if err == nil {
@@ -21,10 +26,15 @@ func (reader *Reader) Read(p []byte) (n int, err error) {
 	return
 }
 
+// Return a new Reader attached to this rope.
 func (rope *Rope) Reader() *Reader {
 	return &Reader{rope: rope}
 }
 
+// ReadAt implements the standard ReadAt interface:
+// it reads len(p) bytes from offset off into p, and returns
+// the number of bytes actually read. If n < len(p), err will
+// explain the shortfall.
 func (rope *Rope) ReadAt(p []byte, off int64) (n int, err error) {
 	for n < len(p) && n < rope.Length() {
 		leaf, at := rope.leafForOffset(int(off))
