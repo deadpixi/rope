814a93633f7bde52518a83f96a91bdb712f36682
Rob King <rob@frigidriver.com>
2022-06-16 17:35:59 -0500

    Comment and cleanup.

--- rope.go
+++ rope.go
@@ -10,20 +10,30 @@ const (
 	balanceFactor = 8
 )
 
+// A Rope is a data structure for storing long runs of text.
+// Ropes are persistent: there is no way to modify an existing rope.
+// Instead, all operations return a new rope with the requested changes.
+//
+// This persistence makes it easy to store old versions of a Rope just by holding on to old roots.
 type Rope struct {
 	content       string
 	length, depth int
 	left, right   *Rope
 }
 
+var empty = &Rope{}
+
+// Return a new empty rope.
 func New() *Rope {
-	return &Rope{}
+	return empty
 }
 
+// Return a new rope with the contents of string s.
 func NewString(s string) *Rope {
 	return &Rope{content: s, length: len(s)}
 }
 
+// Return a new rope that is the concatenation of this rope and the other rope.
 func (rope *Rope) Append(other *Rope) *Rope {
 	switch {
 	case rope.length == 0:
@@ -46,10 +56,12 @@ func (rope *Rope) Append(other *Rope) *Rope {
 	}
 }
 
+// Return a new rope that is the concatenation of this rope and string s.
 func (rope *Rope) AppendString(other string) *Rope {
 	return rope.Append(NewString(other))
 }
 
+// Return a new rope with length bytes at offset deleted.
 func (rope *Rope) Delete(offset, length int) *Rope {
 	if length == 0 || offset == rope.length {
 		return rope
@@ -60,6 +72,7 @@ func (rope *Rope) Delete(offset, length int) *Rope {
 	return left.Append(newRight)
 }
 
+// Returns true if this rope is equal to other.
 func (rope *Rope) Equal(other *Rope) bool {
 	if rope == other {
 		return true
@@ -78,6 +91,7 @@ func (rope *Rope) Equal(other *Rope) bool {
 	return true
 }
 
+// Return the byte at the given index.
 func (rope *Rope) Index(at int) byte {
 	if rope.isLeaf() {
 		return rope.content[at]
@@ -90,6 +104,7 @@ func (rope *Rope) Index(at int) byte {
 	return rope.right.Index(at - rope.left.length)
 }
 
+// Return a new rope with the contents of other inserted at the given index.
 func (rope *Rope) Insert(at int, other *Rope) *Rope {
 	if at == 0 {
 		return other.Append(rope)
@@ -103,14 +118,19 @@ func (rope *Rope) Insert(at int, other *Rope) *Rope {
 	return left.Append(other).Append(right)
 }
 
+// Return a new rope with the contents of string other inserted at the given index.
 func (rope *Rope) InsertString(at int, other string) *Rope {
 	return rope.Insert(at, NewString(other))
 }
 
+// Return the length of the rope in bytes.
 func (rope *Rope) Length() int {
 	return rope.length
 }
 
+// Return a new version of this rope that is balanced for better performance.
+// Generally speaking, this will be invoked automatically during the course of other operations and
+// thus only needs to be called if you know you'll be generating a lot of unbalanced ropes.
 func (rope *Rope) Rebalance() *Rope {
 	if rope.isBalanced() {
 		return rope
@@ -126,24 +146,32 @@ func (rope *Rope) Rebalance() *Rope {
 	return merge(leaves, 0, len(leaves))
 }
 
+// Returns two new ropes, one containing the content to the left of the given index and the other the content to the right.
 func (rope *Rope) Split(at int) (*Rope, *Rope) {
-	if rope.isLeaf() {
+	switch {
+	case rope.isLeaf():
 		return NewString(rope.content[0:at]), NewString(rope.content[at:])
-	}
 
-	if at < rope.left.length {
+	case at == 0:
+		return empty, rope
+
+	case at == rope.length:
+		return rope, empty
+
+	case at < rope.left.length:
 		left, right := rope.left.Split(at)
 		return left, right.Append(rope.right)
-	}
 
-	if at > rope.left.length {
+	case at > rope.left.length:
 		left, right := rope.right.Split(at - rope.left.length)
 		return rope.left.Append(left), right
-	}
 
-	return rope.left, rope.right
+	default:
+		return rope.left, rope.right
+	}
 }
 
+// Return the contents of the rope as a string.
 func (rope *Rope) String() string {
 	if rope.isLeaf() {
 		return rope.content
@@ -227,6 +255,8 @@ func merge(leaves []*Rope, start, end int) *Rope {
 var fibonacci []int
 
 func init() {
+	// The heurstic for whether a rope is balanced depends on the Fibonacci sequence;
+	// we initialize the table of Fibonacci numbers here.
 	first := 0
 	second := 1
 
